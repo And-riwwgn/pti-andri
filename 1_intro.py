dba86c4e0abb9f63ae8ba870dadeb342274f8dd9
And-riwwgn <andriwiguna@students.usu.ac.id>
2024-09-03 12:36:36 +0700

    perkuliahan 1

---
+++ 1_intro.py
@@ -0,0 +1,22 @@
+# deklarsi variabel
+a = float (10.5)
+b = 5
+c = a + b
+print (a)
+print (b)
+print (c)
+
+#operasi (opration) -> tambah (+), kurang (-), kali (*), bagi (/), mudulo (%), pangkat (**)
+a=4
+b=5
+
+perkalian = a*b
+pembagian = a/b 
+pengurangan = a-b
+mudulo = a%b
+pangkat = a**b 
+
+print (perkalian)
+print (pembagian)
+print (mudulo)
+print (pangkat)
